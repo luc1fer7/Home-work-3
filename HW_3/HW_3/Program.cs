95e7fd3acd2b3dc29c7db4519137e183589e1d59
luc1fer7 <92304061+luc1fer7@users.noreply.github.com>
2021-10-11 13:34:51 +0300

    Add files via upload

---
+++ HW_3/HW_3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HW_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
